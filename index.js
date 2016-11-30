1099339b5cf1ab6130b7182ea381f7744f24d676
Eason <onepiece5478@gmail.com>
2016-11-30 18:02:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,21 @@ module.exports = {
         service: [
             { name: 'dInServ', uuid: '0xbb00' },
             { name: 'aInServ', uuid: '0xbb10' },
-            { name: 'pwrAndCurrServ', uuid: '0xbb30' },
+            { name: 'meterServ', uuid: '0xbb30' },
             { name: 'relayServ', uuid: '0xbb40' }
         ],
         characteristic: [
             { name: 'aInConfig', uuid: '0xbb11', params: ['config'], types: ['boolean'] },
             { name: 'aInMeasPeriod', uuid: '0xbb12', params: ['period'], types: ['uint8'] },
-            { name: 'pwrAndCurrConfig', uuid: '0xbb31', params: ['config'], types: ['boolean'] },
-            { name: 'pwrAndCurrMeasPeriod', uuid: '0xbb32', params: ['period'], types: ['uint8'] }
+            { name: 'meterConfig', uuid: '0xbb31', params: ['config'], types: ['boolean'] },
+            { name: 'meterMeasPeriod', uuid: '0xbb32', params: ['period'], types: ['uint8'] }
         ]
      },
      examine: function (periph, basicInfo) {
         var isMine = false;
 
         if (basicInfo.manufacturer === 'sivann' &&
-            basicInfo.model === 'RelayModule' &&
-            basicInfo.fwRev === 'v1.0.0' && 
-            basicInfo.hwRev === 'v1.0.0' &&
-            basicInfo.swRev === 'v1.0.0')
+            basicInfo.devName === 'Power Meter Relay')
             isMine = true;
 
         return isMine;
