7724b163e5dc0cbb9f973044412c6f90c983eb8a
Eason <onepiece5478@gmail.com>
2016-12-15 17:42:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ module.exports = {
         service: [
             { name: 'dInServ', uuid: '0xbb00' },
             { name: 'aInServ', uuid: '0xbb10' },
-            { name: 'meterServ', uuid: '0xbb30' },
+            { name: 'meteringServ', uuid: '0xbb30' },
             { name: 'relayServ', uuid: '0xbb40' }
         ],
         characteristic: [
             { name: 'aInConfig', uuid: '0xbb11', params: ['config'], types: ['boolean'] },
             { name: 'aInMeasPeriod', uuid: '0xbb12', params: ['period'], types: ['uint8'] },
-            { name: 'meterConfig', uuid: '0xbb31', params: ['config'], types: ['boolean'] },
-            { name: 'meterMeasPeriod', uuid: '0xbb32', params: ['period'], types: ['uint8'] }
+            { name: 'meteringConfig', uuid: '0xbb31', params: ['config'], types: ['boolean'] },
+            { name: 'meteringMeasPeriod', uuid: '0xbb32', params: ['period'], types: ['uint8'] }
         ]
      },
      examine: function (periph, basicInfo) {
